6329ac05401d90748c8a6e191adff1e5790c808f
Barend Frederik <barend.frederik@tabcorp.com.au>
2019-05-20 10:23:50 +1000

    setup of template

--- foodcourt-web/src/app/app.module.ts
+++ foodcourt-web/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuTopComponent } from './views/menu/menu-top/menu-top.component';
+import { SignupComponent } from './views/admin/signup/signup.component';
+import { LoginComponent } from './views/admin/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuTopComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
