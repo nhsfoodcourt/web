d3aeeb7e42b991decd8182b237cabb06c37a468e
Barend Frederik <barend.frederik@tabcorp.com.au>
2019-05-20 14:35:37 +1000

    added nav between pages login,signup and password-rec also added componanet password recovery

--- foodcourt-web/src/app/app.module.ts
+++ foodcourt-web/src/app/app.module.ts
@@ -7,21 +7,25 @@ import { MenuTopComponent } from './views/menu/menu-top/menu-top.component';
 import { SignupComponent } from './views/admin/signup/signup.component';
 import { LoginComponent } from './views/admin/login/login.component';
 import { RouterModule } from '@angular/router';
+import { PasswordRecoveryComponent } from './views/admin/password-recovery/password-recovery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuTopComponent,
     SignupComponent,
-    LoginComponent
+    LoginComponent,
+    PasswordRecoveryComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
+      //admin
       { path: '', component: LoginComponent },
       { path: 'signup', component: SignupComponent },
-      { path: 'login', component: LoginComponent }
+      { path: 'login', component: LoginComponent },
+      { path: 'passwordRecovery', component: PasswordRecoveryComponent }
     ])
   ],
   providers: [],
